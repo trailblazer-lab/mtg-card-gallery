b8d036e105535d75b68d716c18bb9407db9f0667
Matheus <174398680+trailblazer-lab@users.noreply.github.com>
2024-07-08 17:39:55 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import reportWebVitals from './reportWebVitals';
 import './App.css';
 
 
+<header> <meta http-equiv="Permissions-Policy" content="interest-cohort=(), user-id=()" />
+ </header>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
